414db1d6707c1fa9cf74c2674409902716f00530
Muhammad Farhan <m.farhankz@gmail.com>
2023-09-05 01:48:12 +0500

    theme setting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,11 @@
 import type { Config } from 'tailwindcss'
 
+let brandConfig;
+if (process.env.VERSION) {
+  brandConfig = require(`./src/brands/${process.env.VERSION}/config`);
+}
+
+
 const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,13 +13,32 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      colors: {
+        primary: brandConfig.colors.primary,
+        secondary: brandConfig.colors.secondary,
+        header: brandConfig.colors.header,
+        black: '#000',
+        white: '#fff',
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
