f9f752b5e9131b8640d6ad91818fe09282aa7596
Muhammad Farhan <m.farhankz@gmail.com>
2023-09-06 20:21:10 +0500

    first commit

--- src/app/public/login/page.tsx
+++ src/app/public/login/page.tsx
@@ -5,7 +5,7 @@ import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
 
 function Login() {
-  const [visible, setVisible] = useState<boolean>(false);
+    const [visible, setVisible] = useState<boolean>(false);
 
     return (
         <div className="card flex justify-content-center">
@@ -13,10 +13,7 @@ function Login() {
 
             <Dialog header="Header" visible={visible} style={{ width: '30vw' }} onHide={() => setVisible(false)}>
                 <p className="m-0">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    Login
                 </p>
             </Dialog>
         </div>
