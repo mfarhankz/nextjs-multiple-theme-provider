4007ebf6d3bceb928164c57a7ba9011845d36897
Muhammad Farhan <m.farhankz@gmail.com>
2023-09-20 04:03:43 +0500

    fix the main issue of logo path

--- src/app/admin/components/header/index.tsx
+++ src/app/admin/components/header/index.tsx
@@ -12,9 +12,8 @@ function Header() {
         <nav className="border-gray-200">
           <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2.5">
             <Link href="/dashboard" className="flex items-center">
-              {configuration.logo}
               <Image
-                src={configuration.logo}
+                src={configuration?.logo}
                 alt="Brand Logo"
                 width={190}
                 height={50}
@@ -28,21 +27,7 @@ function Header() {
               aria-expanded="false"
             >
               <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
+            
             </button>
             <div
               className="hidden w-full md:block md:w-auto"
@@ -65,21 +50,7 @@ function Header() {
                     className="flex items-center justify-between w-full py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
                   >
                     Dropdown
-                    <svg
-                      className="w-2.5 h-2.5 ml-2.5"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 10 6"
-                    >
-                      <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="m1 1 4 4 4-4"
-                      />
-                    </svg>
+                    
                   </button>
                   <div
                     id="dropdownNavbar"
--- src/configuration/index.ts
+++ src/configuration/index.ts
@@ -1,7 +1,8 @@
-const brandConfig = require(`../brands/${process.env.VERSION}`).default;
+const brandConfig = require(`../brands/${process.env.NEXT_PUBLIC_VERSION || 'default'}`).default;
 
 const config = {
    ...brandConfig,
-   logo: `/${process.env.VERSION}_logo.png`,
-}
+   logo: `/${process.env.NEXT_PUBLIC_VERSION || 'default'}_logo.png`,
+};
+
 export default config;
