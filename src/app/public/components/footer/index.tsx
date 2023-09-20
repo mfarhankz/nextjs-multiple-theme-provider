0d811f4a4d0012fc31f25b1b39981f6b0665d032
Muhammad Farhan <m.farhankz@gmail.com>
2023-09-20 16:11:18 +0500

    header of puclic

--- src/app/public/components/footer/index.tsx
+++ src/app/public/components/footer/index.tsx
@@ -20,14 +20,42 @@ function Footer() {
         </div>
       </header> */}
       <header>
-        <div className="container px-40">
+        <div className="container px-40 py-10">
           <div className="row-flex">
-            <div className='w-1/2'>
-              <div>
-                <div className='bg-[#F8FAE7]'>
+            <div className='w-1/2 p-4'>
+              <div className='w-1/2 py-4'>
+                <div className='bg-[#F8FAE7] drop-shadow-md rounded-full flex justify-center '>
                   TALKING INTELLIGENCE 👌🏻
                 </div>
               </div>
+              <div className='w-full py-4'>
+                <h1 className='text-7xl font-semibold text-sky-950'>
+                  <span>Talking Code,</span>
+                  <span className='underline decoration-pink-300'> Unlimited </span>
+                  <span> Chat.</span>
+                </h1>
+              </div>
+              <div>
+                <p className='text-slate-500 text-xl mt-3'>
+                  Equipped with human-like intelligence, our chatbots establish fluent and interactive dialogues.
+                </p>
+              </div>
+              <div className='gap-5 flex mt-7'>
+                <button className='py-[18px] flex px-[30px] hover:bg-opacity-95 bg-blue-950 text-white rounded-full'>
+                  <p className='text-sm'>Get Started</p>
+                  <svg className='mt-1 ml-3 h-3' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" fill='#fff' /></svg>
+                </button>
+                <button className='py-[18px] flex px-[30px] hover:border-2  hover:border-blue-950 text-blue-950 bg-white rounded-full'>
+                  <p className='text-sm'>Get Started</p>
+                  <svg className='mt-1 ml-3 h-3' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" /></svg>
+                </button>
+              </div>
+              <div>
+                <p className='text-xs mt-7 ms-5 font-light text-gray-400'>
+                  Try for free. No credit card required.
+                </p>
+              </div>
+
             </div>
           </div>
         </div>
