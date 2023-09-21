b90077b0a566192a97c068e8f5d762ae8c10b86d
Muhammad Farhan <m.farhankz@gmail.com>
2023-09-21 16:05:59 +0500

    header

--- src/app/public/components/footer/index.tsx
+++ src/app/public/components/footer/index.tsx
@@ -4,93 +4,54 @@ import React from 'react'
 function Footer() {
   return (
     <>
-      {/* <header className='bg-blue-500 lg:h-[625px] md:h-[420px] h-[580px]'>
-        <div className='text-center flex justify-center'>
-          <div className='lg:w-7/12 w-full lg:mt-[120px] md:mt-[120px] mt-[20px]'>
-            <h2 className='lg:text-[50px] text-[36px] text-white'>
-              Work smarter, not harder!
-            </h2>
-            <p className='my-5 text-white text-[20px]'>
-              ReboGateway offers fresh and unique ideas that will undoubtedly create a new path and source of revenue for your business.
-            </p>
-            <button className='px-[40px] py-[15px] bg-[#00C997] rounded text-white'>
-              CREATE AN ACCOUNT
-            </button>
-          </div>
-        </div>
-      </header> */}
       <header>
-        <div className="container px-40 py-10">
-          <div className="row-flex">
-            <div className='w-1/2 p-4'>
-              <div className='w-1/2 py-4'>
-                <div className='bg-[#F8FAE7] drop-shadow-md rounded-full flex justify-center '>
+        <div className="lg:px-32 lg:py-10">
+          <div className=' lg:flex'>
+            <div className='lg:w-1/2 w-full p-4'>
+              <div className='py-4 flex justify-center lg:justify-normal'>
+                <div className='bg-[#F8FAE7] py-1 px-5 text-[13px]  drop-shadow-md rounded-full flex justify-center '>
                   TALKING INTELLIGENCE 👌🏻
                 </div>
               </div>
               <div className='w-full py-4'>
-                <h1 className='text-7xl font-semibold text-sky-950'>
+                <h1 className='lg:text-7xl md:text-7xl text-5xl lg:text-start text-center font-semibold text-sky-950'>
                   <span>Talking Code,</span>
                   <span className='underline decoration-pink-300'> Unlimited </span>
                   <span> Chat.</span>
                 </h1>
               </div>
               <div>
-                <p className='text-slate-500 text-xl mt-3'>
+                <p className='text-slate-500 text-xl mt-3 lg:text-start text-center'>
                   Equipped with human-like intelligence, our chatbots establish fluent and interactive dialogues.
                 </p>
               </div>
-              <div className='gap-5 flex mt-7'>
-                <button className='py-[18px] flex px-[30px] hover:bg-opacity-95 bg-blue-950 text-white rounded-full'>
-                  <p className='text-sm'>Get Started</p>
-                  <svg className='mt-1 ml-3 h-3' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" fill='#fff' /></svg>
-                </button>
-                <button className='py-[18px] flex px-[30px] hover:border-2  hover:border-blue-950 text-blue-950 bg-white rounded-full'>
-                  <p className='text-sm'>Get Started</p>
-                  <svg className='mt-1 ml-3 h-3' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" /></svg>
-                </button>
+              <div className='flex justify-center lg:justify-normal'>
+                <div className='gap-5 md:flex mt-7'>
+                  <button className='py-[18px] flex px-[30px] hover:bg-opacity-95 mt-3 bg-blue-950 text-white rounded-full'>
+                    <p className='text-sm'>Get Started</p>
+                    <svg className='mt-1 ml-3 h-3' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" fill='#fff' /></svg>
+                  </button>
+                  <button className='py-[18px] flex px-[30px] hover:border-2  mt-3 hover:border-blue-950 text-blue-950 bg-white rounded-full'>
+                    <p className='text-sm'>Explore Hub</p>
+                    <svg className='mt-1 ml-3 h-3' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z" /></svg>
+                  </button>
+                </div>
               </div>
               <div>
-                <p className='text-xs mt-7 ms-5 font-light text-gray-400'>
+                <p className='text-xs text-center lg:text-start mt-7 ms-5 font-light text-gray-400'>
                   Try for free. No credit card required.
                 </p>
               </div>
 
             </div>
+            <div className='lg:w-1/2 w-full flex lg:items-center justify-center md:mt-5'>
+              <img className='h-96 w-144' src="./ipad.png" alt="" />
+            </div>
           </div>
+
         </div>
       </header>
-      {/* <footer>
-        <div className='grid grid-cols-2 gap-3 md:text-center'>
-          <div>
-            <div className=''>
-              <ul className='lg:flex md:flex ms-32 list-none'>
-                <li className='sm:mb-4 me-2'>HOME</li>
-                <li className='sm:mb-4 me-2'>ABOUT</li>
-                <li className='sm:mb-4 me-2'>TOOLS</li>
-                <li className='sm:mb-4 me-2'>PRODUCTS</li>
-                <li className='sm:mb-4 me-2'>CONTACT</li>
-              </ul>
-            </div>
-            <div>
-              <ul className='lg:flex md:flex ms-32 list-none'>
-                <li className='sm:mb-4 me-5'>
-                  <a href="#">
-                    Terms and Condition
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    Privacy Policy
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div>
-          </div>
-        </div>
-      </footer> */}
+
     </>
   )
 }
--- src/app/public/components/header/index.tsx
+++ src/app/public/components/header/index.tsx
@@ -6,34 +6,34 @@ import Login from '@/app/public/login/page';
 
 const Header = () => {
   return (
-    <header className="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full text-sm mt-10 py-5 px-12 bg-red-600">
-      <nav className="max-w-[85rem] w-full mx-auto lg:pl-9 flex lg:justify-around sm:items-center justify-between" aria-label="Global">
+    <header className="flex flex-wrap lg:px-32 justify-start sm:flex-nowrap z-50 w-full text-sm mt-10 py-[10px] px-12">
+      <nav className="max-w-[85rem] w-full mr-10 flex lg:justify-around sm:items-center justify-between" aria-label="Global">
         <div className="flex items-center justify-between">
-          <a className="flex title-font font-medium items-center text-gray-900">
+          <a className="flex title-font w-40 font-medium items-center text-gray-900">
             <Image src={configuration.logo} alt="Brand Logo" width={190} height={50} />
           </a>
 
         </div>
         <div id="navbar-with-collapse" className="hidden basis-full grow lg:block">
           <div className="flex sm:flex-row sm:items-center sm:justify-end md:justify-between sm:mt-0 sm:pl-5">
-            <div className='flex lg:gap-8 ms-6'>
-              <a className="font-light text-[#272657] text-1xl font-normal" href="#" aria-current="page">Home</a>
-              <a className="font-light text-[#272657] text-1xl font-normal" href="#">Features<span className="bg-[#EB9AA029] ms-2 text-xs font-medium mr-2 px-3 py-1 rounded">New</span></a>
-              <a className="font-light text-[#272657] text-1xl font-normal" href="#">Process</a>
-              <a className="font-light text-[#272657] text-1xl font-normal" href="#">Pricing</a>
-              <a className="font-light text-[#272657] text-1xl font-normal" href="#">Blog</a>
+            <div className='flex items-end ms-6'>
+              <a className="font-light lg:pr-10 text-[#272657] text-1xl" href="#" aria-current="page">Home</a>
+              <a className="font-light lg:pr-10 text-[#272657] text-1xl" href="#">Features<span className="bg-[#EB9AA029] text-[#EB9AA0] ms-2 text-xs font-medium  px-3 py-1 rounded">New</span></a>
+              <a className="font-light lg:pr-10 text-[#272657] text-1xl" href="#">Process</a>
+              <a className="font-light lg:pr-10 text-[#272657] text-1xl" href="#">Pricing</a>
+              <a className="font-light lg:pr-10 text-[#272657] text-1xl" href="#">Blog</a>
             </div>
             <div className='flex'>
               <button className="flex justify-center items-center py-1 px-3 hover:border  hover:border-black rounded-full text-normal w-[90px]">
                 Log in
               </button>
-              <button className="inline-flex items-center justify-center border py-1 px-3 rounded-full w-[200px] h-[50px] text-normal">
+              <button className="items-center justify-center bg-[#E7F8F8] text-[#5EE9E8] py-1 px-3 hover:border-2 hover:border-[#5EE9E8] rounded-full w-[200px] h-[50px] text-normal">
                 Join the Community
               </button>
             </div>
           </div>
         </div>
-        <div>
+        <div className='flex items-center'>
           <button type="button" className="hs-collapse-toggle lg:hidden p-2 inline-flex gap-2 rounded-md font-medium bg-white text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none item-ceter focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800 ps-auto" data-hs-collapse="#navbar-with-collapse" aria-controls="navbar-with-collapse" aria-label="Toggle navigation">
             <svg className="hs-collapse-open:hidden w-4 h-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
