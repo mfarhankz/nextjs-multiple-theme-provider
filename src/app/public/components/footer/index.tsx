d674dc2d1d92a4069918cb552cc6e14d169d1b6f
Muhammad Farhan <m.farhankz@gmail.com>
2023-10-03 22:02:23 +0500

    footer

--- src/app/public/components/footer/index.tsx
+++ src/app/public/components/footer/index.tsx
@@ -4,7 +4,17 @@ import React from 'react'
 function Footer() {
   return (
     <>
-
+      <footer>
+        <div className="container lg:px-32">
+          <div>
+            <div className='lg:w-9/12 p-5'>
+              <p className='text-base'>
+                At our company, we are dedicated to supporting you in the seamless creation of exceptional content that is both efficient and high in quality. Our expert team and advanced AI-powered tools work hand in hand to assist you in effortlessly producing top-notch content that captivates your audience.
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
 
     </>
   )
